c401ff622a94d8df2b5ab7eeafe4ec72dab90018
Jhonathan <Jhonathandepaula3@gmail.com>
2026-05-15 17:16:39 -0300

    Correção Render

--- app.py
+++ app.py
@@ -345,4 +345,8 @@ def boas_vindas():
 
 # ▶️ RODAR SERVIDOR
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
